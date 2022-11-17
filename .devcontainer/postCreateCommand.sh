3a12ba842ec8a8135fed1981a652968135b6a285
Andre Light <andrelight.al@gmail.com>
2022-11-16 17:34:43 -0700

    Add node.js and Heroku CLI install script
    - Set Node version to `lts/*` to install with dev container.
    - Run `postCreateCommand` in interactive shell (`bash -i ...`).
    - Add command to `postCreateCommand` to install Heroku CLI.
      - This installation requires node.js.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,7 @@ cd /workspace
 rm -rf ./src/.vscode
 cp -R ./.devcontainer/.vscode ./src
 cp -R ./.devcontainer/.pryrc /home/vscode/
+
+# Heroku CLI w/ auto-update
+# Guide: https://devcenter.heroku.com/articles/heroku-cli
+curl https://cli-assets.heroku.com/install.sh | sh
