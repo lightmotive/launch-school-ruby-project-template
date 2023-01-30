e1596aaa0ee3ce5908e0fb7ebe811c1baa77c23c
Andre Light <andrelight.al@gmail.com>
2023-01-30 12:49:15 -0700

    Add Postgres default connection commands
    - `dcreatedb` and `ddropdb`.
    - Abstract command builder to `pgcmd_with_conn_args`.

--- .devcontainer/.devcontainer_commands.sh
+++ .devcontainer/.devcontainer_commands.sh
@@ -1,15 +1,35 @@
 #!/bin/zsh
 
+function pgcmd_with_conn_args() {
+  cmd=$1
+
+  printf "Executing w/ default args & '~/.pgpass':\n"
+  printf "$cmd ${PGCMD_CONN_ARGS[@]} ${@:2:99}"
+  printf '\n'
+  cmds=($cmd "${PGCMD_CONN_ARGS[@]}" "${@:2:99}")
+  "${cmds[@]}"
+}
+
 function dpsql() {
+  cmd="psql"
+
   if [[ $1 == "--show-command" ]]; then
-    printf 'Connect to psql (~/.pgpass): '
-    printf '%q ' "${DPSQL_CONNECT[@]}"
+    printf "That executes the following (user password configured in ~/.pgpass):"
+    printf "\n$cmd "
+    printf '%q ' "${PGCMD_CONN_ARGS[@]}"
+    printf '\n\n'
+    printf "Other commands that use default conn args: dcreatedb, ddropdb"
     printf '\n'
     return 0
   fi
 
-  # Execute default `psql` connect command with params appended
-  DPSQL_CONNECT+=("${@:1:99}")
-  echo "[dpsql w/ (~/.pgpass)]: ${DPSQL_CONNECT[@]}"
-  "${DPSQL_CONNECT[@]}"
+  pgcmd_with_conn_args $cmd "${@:1:99}"
+}
+
+function dcreatedb() {
+  pgcmd_with_conn_args 'createdb' "${@:1:99}"
+}
+
+function ddropdb() {
+  pgcmd_with_conn_args 'dropdb' "${@:1:99}"
 }
