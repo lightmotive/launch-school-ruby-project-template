52039bd6d5521238433198a41b5ab6c08e13f9e3
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-11 20:28:20 -0700

    Permanently resolve "dubious ownership" error
    - Resolve Git commit error, "detected dubious ownership in repository".
      - Details: https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/

--- .devcontainer/postCreateCommandCustom.sh
+++ .devcontainer/postCreateCommandCustom.sh
@@ -6,6 +6,3 @@
 # - Get your private GitHub email from https://github.com/settings/emails.
 # git config --global user.email "private+email@users.noreply.github.com"
 # git config --global user.name "Your Name (or username)"
-
-# Allow known repos with locally renamed directories
-# git config --global --add safe.directory $(readlink -e renamed_dir)
